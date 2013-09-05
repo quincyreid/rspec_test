b2488dbdd2d724af14b14cb17c8306d0ba2030f1
Quincy Reid <qreid51@gmail.com>
2013-09-05 02:51:21 -0400

    Create first test

---
+++ spec/book_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Book do 
+  
+  before :each do
+    @book = Book.new "Title", "Author", :category
+  end
+  
+  describe "#new" do 
+    it "returns a new book object" do
+      @book.should be_an_instance_of Book
+    end
+  
+  end
+  
+end 
